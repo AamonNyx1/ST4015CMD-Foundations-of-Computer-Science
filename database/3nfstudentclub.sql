02936e85dcf67b00375ed01e5a2eed26836bd782
AamonNyx1 <fan314480@gmail.com>
2026-03-04 12:19:42 +0545

    Create 3nfstudentclub.sql with club membership data
    Added a new SQL file containing student club membership data.

---
+++ database/3nfstudentclub.sql
@@ -0,0 +1,15 @@
++-------------+-------------+------------+    
+| StudentName | ClubName    | JoinDate   |     
++-------------+-------------+------------+   
+| Asha        | Music Club  | 2024-01-10 | 
+| Nisha       | Music Club  | 2024-01-20 |   
+| Suman       | Music Club  | 2024-01-22 |  
+| Bikash      | Sports Club | 2024-01-12 |   
+| Asha        | Sports Club | 2024-01-15 |    
+| Pooja       | Sports Club | 2024-01-27 |   
+| Rohan       | Drama Club  | 2024-01-18 |  
+| Bikash      | Drama Club  | 2024-01-25 |   
+| Nisha       | Coding Club | 2024-01-28 |
+| Aman        | Coding Club | 2024-01-30 |  
++-------------+-------------+------------+   
+10 rows in set (0.00 sec)                                                                                                                                                                      
