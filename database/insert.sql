215bae4b774ec8c5e16beac460d490b8b7d45139
AamonNyx1 <fan314480@gmail.com>
2026-03-05 22:43:48 +0545

    Update insert.sql

--- database/insert.sql
+++ database/insert.sql
@@ -1,4 +1,6 @@
 INSERT INTO Student VALUES (8, 'Srijan', 'srijan@email.com');
+INSERT INTO Student VALUES (9, 'mello', 'mello@email.com');
 INSERT INTO Club VALUES (501, 'Hacking Club', 'R699', 'Ms. Piya');
+INSERT INTO Club VALUES (502, 'Hacking Club', 'R669', 'Ms. Sia');
 SELECT * FROM Student;
 SELECT * FROM Club;
