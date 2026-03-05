a446e5cd3101cf1c06d514e39c3a3cdf831da11b
AamonNyx1 <fan314480@gmail.com>
2026-03-05 22:36:51 +0545

    Create insertOutput.sql

---
+++ database/insertOutput.sql
@@ -0,0 +1,25 @@
++-----------+-------------+------------------+
+| StudentID | StudentName | Email            |
++-----------+-------------+------------------+
+|         1 | Asha        | asha@email.com   |
+|         2 | Bikash      | bikash@email.com |
+|         3 | Nisha       | nisha@email.com  |
+|         4 | Rohan       | rohan@email.com  |
+|         5 | Suman       | suman@email.com  |
+|         6 | Pooja       | pooja@email.com  |
+|         7 | Aman        | aman@email.com   |
+|         8 | Srijan      | srijan@email.com |
+|         9 | mello       | mello@email.com  |
++-----------+-------------+------------------+
+9 rows in set (0.00 sec)
++--------+--------------+----------+------------+
+| ClubID | ClubName     | ClubRoom | ClubMentor |
++--------+--------------+----------+------------+
+|    101 | Music Club   | R101     | Mr. Raman  |
+|    202 | Sports Club  | R202     | Ms. Sita   |
+|    303 | Drama Club   | R303     | Mr. Kiran  |
+|    401 | Coding Club  | Lab1     | Mr. Anil   |
+|    501 | Hacking Club | R699     | Ms. Piya   |
+|    502 | Hacking Club | R669     | Ms. Sia    |
++--------+--------------+----------+------------+
+6 rows in set (0.00 sec)
